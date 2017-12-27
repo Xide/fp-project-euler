ec8a7f32adb4ae12cc8d08ecfc8dae6b9f2a64ca
Germain <germain.gau@gmail.com>
2017-12-28 00:46:09 +0100

    removed answers test from unit tests

--- src/ex01_spec.js
+++ src/ex01_spec.js
@@ -6,7 +6,4 @@ describe("Problem 1 - Multiples of 3 and 5", function () {
   it("Should yield 23 when the high limit is 10", function () {
     expect(ex01.sumOfMultiplesUntil(10)).to.equals(23);
   })
-  // it("Should yield the correct result for the exercice", function () {
-  //   expect(ex01.sumOfMultiplesUntil(1000)).to.equals(233168);
-  // })
 })
--- src/ex02_spec.js
+++ src/ex02_spec.js
@@ -4,12 +4,19 @@ var expect = require("chai").expect;
 var ex = require('./ex02')
 
 describe("Problem 2 - Even Fibonacci numbers", function () {
-  it("Should yield the correct fibonnaci sequence for the 10 first terms", function () {
-    expect(R.map(ex.fibAt, R.range(0, 10))).to.deep.equals(
-      [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    )
+  describe("fibAt", function() {
+    it("Should yield the correct fibonnaci sequence for the 10 first terms", function () {
+      expect(R.map(ex.fibAt, R.range(0, 10))).to.deep.equals(
+        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+      )
+    })
   })
-  // it("Should yield the correct result for the exercice", function () {
-  //   expect(ex.evenFibonacciNumbersUntil(4000000)).to.equals(4613732);
-  // })
+  describe("fibUntil", function() {
+    it("Should not print anything when max is 0", function() {
+      expect(ex.fibUntil(0)).to.deep.equals([])
+    })
+    it("Should not print values above the max", function() {
+      expect(ex.fibUntil(64)).to.deep.equals([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+  })
 })
