668cb00b13d83ac5f3b03c4a9d622357643f2b2d
Germain <germain.gau@gmail.com>
2018-01-06 04:42:49 +0100

    added subject in comments

--- src/ex03.js
+++ src/ex03.js
@@ -1,3 +1,9 @@
+// URL: https://projecteuler.net/problem=3
+// Subject:
+// The prime factors of 13195 are 5, 7, 13 and 29.
+//
+// What is the largest prime factor of the number 600851475143 ?
+
 import R from 'ramda';
 import { primeFactors } from './utils';
 
--- src/ex05.js
+++ src/ex05.js
@@ -1,5 +1,14 @@
+// URL: https://projecteuler.net/problem=5
+// Subject:
+// 2520 is the smallest number that can be divided by
+// each of the numbers from 1 to 10 without any remainder.
+//
+// What is the smallest positive number that is evenly
+// divisible by all of the numbers from 1 to 20?
+
 import Lazy from 'lazy.js';
 import R from 'ramda';
+
 
 // Int -> Int -> Lazy.Sequence -> Lazy.Sequence
 const _evenRangeFilter = R.curry(
